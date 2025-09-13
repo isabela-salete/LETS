21643478e76194ea155994b4f68f94a57c4178c0
isabela-salete <118233286+isabela-salete@users.noreply.github.com>
2025-09-12 21:10:41 -0300

    Update CAT.py

--- pages/CAT.py
+++ pages/CAT.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import json
+from unidecode import unidecode
+import re
+
+st.set_page_config(layout='wide')
+
+st.title("Teste - CAT")
+
+st.pdf('CAT.pdf', height=450)
 
